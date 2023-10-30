a2c3818928d9aca9b3eb5cf742312b5fd3505167
Number_A <68718087+Number-A@users.noreply.github.com>
2023-10-30 16:02:24 -0400

    restructuring

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -2,28 +2,70 @@ import styles from "./App.module.css";
 
 export default function App() {
   return (
-    <div className="home-layout">
-      <div className={styles.menuBar}>
-        <img className={styles.logo} src="/logo192.png" />
-        <div className={styles.btns}>
-          <a className={styles.menuItem} href="#">
-            menu item 1
-          </a>
-          <a className={styles.menuItem} href="#">
-            menu item 2
-          </a>
-          <a className={styles.menuItem} href="#">
-            menu item 3
-          </a>
-        </div>
-      </div>
-      <header className={styles.poster}>
-        <h1>Theo Badele</h1>
-      </header>
-      <div className="quote">Quote</div>
-      <div className="personalies">Peronalities</div>
-      <div className="carousel">Carousel</div>
-      <div className="footer">Footer</div>
+    <div className={styles.homeLayout}>
+      <NotificationBar />
+      <Header>
+        <Title></Title>
+      </Header>
+      <NavigationMenu></NavigationMenu>
+      <span>content</span>
+      <span>main content</span>
+      <content>content</content>
+      <footer>Footer</footer>
+    </div>
+  );
+}
+
+function NavigationMenu() {
+  return (
+    <div className={styles.navigationMenuBox}>
+      <nav className={styles.navigationMenu}>
+        <ul className={styles.navigationMenuItems}>
+          <li>
+            <a href="#">Author</a>
+          </li>
+          <li>
+            <a href="#">Developer</a>
+          </li>
+          <li>
+            <a href="#">Gamer</a>
+          </li>
+          <li className={styles.navigationMenuDropdown}>
+            <select>
+              <option value="story">Story</option>
+              <option value="interest">Interest</option>
+              <option value="ambition">Ambitions</option>
+            </select>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+function NotificationBar() {
+  return (
+    <div className={styles.notificationBar}>
+      <a className={styles.notificationLink}>Join my discord</a>
+    </div>
+  );
+}
+
+function Header({ children }) {
+  return (
+    <div className={styles.headerBox}>
+      <header>{children}</header>
+    </div>
+  );
+}
+
+function Title() {
+  return (
+    <div className={styles.mainTitle}>
+      <h1 className={styles.mainTitleText}>
+        <span>Theodor Badele</span>
+        <span>Developer, minecraft player and writer</span>
+      </h1>
     </div>
   );
 }
